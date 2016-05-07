651107fc2a4fdd28e962ac67fd630e3e76ffcae3
shlok007 <shlok.srivastava15@gmail.com>
2016-05-07 16:35:17 -0400

    Added request model,bug fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,22 @@
-class PostsController < ApplicationController
-	$post=Post.new
-	def create
- 		@post=Post.new(post_params)
- 	 	 if @post.save
- 	 	 	redirect_to root_path
- 	 	 else
- 	 	 	render json: {"error":"post not saved"}
- 	 	 end
- 	end
+	class PostsController < ApplicationController
+		$post=Post.new
+		def create
+	 		@post=Post.new(post_params)
+	 	 	 if @post.save
+	 	 	 	redirect_to root_path
+	 	 	 else
+	 	 	 	render json: {"error":"post not saved"}
+	 	 	 end
+	 	end
  	def apply
- 		@post=$post
- 		@post.assigned_to=current_user.full_name
- 		@post.save
- 		redirect_to :back
+ 		if user_signed_in?
+ 			@post=$post
+ 			@post.assigned_to=current_user.full_name
+ 			@post.save
+ 			redirect_to :back
+ 		else
+ 			redirect_to new_user_session_path
+ 		end
  	end
  	def post_params
  		params.require(:post).permit(:title,:description,:category,:user_id)
